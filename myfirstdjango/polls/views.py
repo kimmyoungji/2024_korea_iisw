fe855b841eb7e1d2afd4181503545a3b4a7e05eb
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-03-08 23:59:03 +0900

    recursion 사용을 지양해야하는 이유, 빅데이터 분석시 컴프레헨션 사용주의해야하는 이유, 빅데이터 분석에 함수형 프로그래밍이 필요한 이유, closure, first class 와 higher-order

--- myfirstdjango/polls/views.py
+++ myfirstdjango/polls/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world. you are in polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    # 예외 처리 중요
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {"question": question}) 
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoseNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "선택을 하세요!"
+        })
+    else: 
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results'), args=(question_id,))
